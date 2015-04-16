b2f5d17fe1c3f117b4a952611f41e1c0ba5ddccb
Karthik Gururaj <karthik.gururaj@intel.com>
2015-04-16 15:01:35 -0700

    Added variable #inputs summer tool

---
+++ tools/sum/summer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys;
+import time;
+
+if(len(sys.argv) < 3):
+    sys.stderr.write("Needs at least 2 arguments : <input_vector_file> <output_filename>\n");
+    sys.exit(-1);
+
+sum=0;
+for filename in sys.argv[1:len(sys.argv)-2]:
+    fptrS=open(filename, "r");
+    for line in fptrS:
+        line = line.rstrip();
+        val = int(line);
+        sum += val;
+    fptrS.close();
+
+fptrO=open(sys.argv[len(sys.argv)-1],"w");
+fptrO.write("%d\n"%(sum));
+fptrO.close();
+
+time.sleep(20);
+
+
