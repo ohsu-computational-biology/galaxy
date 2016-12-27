516af3ebfe0a3e0465739088339c10ce7568c901
Alex Buchanan <buchanae@ohsu.edu>
2016-12-27 08:17:44 -0800

    Clean up TES job runner. Still a WIP.

--- lib/galaxy/jobs/runners/tes.py
+++ lib/galaxy/jobs/runners/tes.py
@@ -4,8 +4,9 @@ Job control via TES.
 import logging
 import os
 import json
-import urllib
 import shlex
+
+import requests
 
 from galaxy import model
 from galaxy.jobs.runners import (
@@ -25,9 +26,9 @@ class TESJobState( AsynchronousJobState ):
         Encapsulates state related to a job.
         """
         super( TESJobState, self ).__init__( **kwargs )
-        self.failed = False
         self.user_log = None
         self.user_log_size = 0
+        self.cleanup_file_attributes = ['output_file', 'error_file', 'exit_code_file']
 
 
 class TESJobRunner( AsynchronousJobRunner ):
@@ -42,8 +43,25 @@ class TESJobRunner( AsynchronousJobRunner ):
         self._init_monitor_thread()
         self._init_worker_threads()
 
+    def _send_task(self, master_addr, task):
+        url = 'http://' + master_addr + '/v1/jobs'
+        r = requests.post(url, json=task)
+        data = r.json()
+        job_id = data['value']
+        return job_id
+
+    def _get_job(self, master_addr, job_id):
+        url = 'http://' + master_addr + '/v1/jobs/' + job_id
+        r = requests.get(url)
+        return r.json()
+
+    def _cancel_job(self, master_addr, job_id):
+        # TODO TES doesn't actually shutdown running jobs.
+        url = 'http://' + master_addr + '/v1/jobs/' + job_id
+        r = requests.delete(url)
+
     def queue_job( self, job_wrapper ):
-        """Create job script and submit it to the DRM"""
+        """Submit the job to TES."""
 
         # prepare the job
         include_metadata = asbool( job_wrapper.job_destination.params.get( "embed_metadata_in_job", True ) )
@@ -52,15 +70,14 @@ class TESJobRunner( AsynchronousJobRunner ):
 
         job_destination = job_wrapper.job_destination
         galaxy_id_tag = job_wrapper.get_id_tag()
-
         master_addr = job_destination.params.get("tes_master_addr")
-
         container = job_wrapper.tool.containers[0]
+
         print '=' * 50
         print job_wrapper.command_line
         print
 
-        task = {
+        job_id = self._send_task(master_addr, {
             "name" : job_wrapper.tool.name,
             "projectId" : "MyProject",
             "description" : job_wrapper.tool.description,
@@ -73,52 +90,29 @@ class TESJobRunner( AsynchronousJobRunner ):
                     "stderr" : "stderr",
                 }
             ]
-        }
+        })
 
-        u = urllib.urlopen('http://' + master_addr + "/v1/jobs", json.dumps(task))
-        data = json.loads(u.read())
-        job_id = data['value']
-
-        cjs = TESJobState(
+        job_state = TESJobState(
             job_id=job_id,
             files_dir=job_wrapper.working_directory,
             job_wrapper=job_wrapper
         )
-        cjs.cleanup_file_attributes = ['output_file', 'error_file', 'exit_code_file']
-
-
-        #script = self.get_job_file(
-            #job_wrapper,
-            #exit_code_path=cjs.exit_code_file,
-            #slots_statement=galaxy_slots_statement,
-        #)
-        #try:
-            #self.write_executable_script( executable, script )
-        #except:
-            #job_wrapper.fail( "failure preparing job script", exception=True )
-            #log.exception( "(%s) failure preparing job script" % galaxy_id_tag )
-            #return
-
-        #cleanup_job = job_wrapper.cleanup_job
-        #try:
-            #open(submit_file, "w").write(submit_file_contents)
-        #except Exception:
-            #if cleanup_job == "always":
-                #cjs.cleanup()
-                # job_wrapper.fail() calls job_wrapper.cleanup()
-            #job_wrapper.fail( "failure preparing submit file", exception=True )
-            #log.exception( "(%s) failure preparing submit file" % galaxy_id_tag )
-            #return
-
-        # submit task
-        #resp = TES.submit(task)
 
         log.info( "(%s) queued as %s" % ( galaxy_id_tag, job_id ) )
-
-        # TODO is this necessary?
         job_wrapper.set_job_destination( job_destination, job_id )
+        self.monitor_job(job_state)
 
-        self.monitor_job(cjs)
+    def _concat_job_log(self, data, key):
+        s = ''
+        for i, log in enumerate(data['logs']):
+            s += 'Step #{}\n'.format(i)
+            s += log.get(key, '')
+        return s
+
+    def _concat_exit_codes(self, data):
+        # TODO TES doesn't actually return the exit code yet
+        return '0'
+        return ','.join([str(l['exitcode']) for l in data['logs']])
 
     def check_watched_item( self, job_state ):
         """
@@ -129,27 +123,25 @@ class TESJobRunner( AsynchronousJobRunner ):
         galaxy_id_tag = job_state.job_wrapper.get_id_tag()
         master_addr = job_state.job_wrapper.job_destination.params.get("tes_master_addr")
 
-        r = urllib.urlopen("http://" + master_addr + "/v1/jobs/%s" % (job_id))
-        data = json.loads(r.read())
+        data = self._get_job(master_addr, job_id)
         state = data['state']
         job_running = state == "Running"
         job_complete = state == "Complete"
         job_failed = "Error" in state
 
-        #try:
-            #s1, s4, s7, s5, s9, log_size = summarize_condor_log(job_state.user_log, job_id)
-        #except Exception:
-            # so we don't kill the monitor thread
-            #log.exception( "(%s/%s) Unable to check job status" % ( galaxy_id_tag, job_id ) )
-            #log.warning( "(%s/%s) job will now be errored" % ( galaxy_id_tag, job_id ) )
-            #job_state.fail_message = "Cluster could not complete job"
-            #self.work_queue.put( ( self.fail_job, job_state ) )
-            #continue
+        print '=' * 50
+        print state, job_state.running, data
+
+        if job_running and job_state.running:
+            return job_state
 
         if job_running and not job_state.running:
             log.debug( "(%s/%s) job is now running" % ( galaxy_id_tag, job_id ) )
             job_state.job_wrapper.change_state( model.Job.states.RUNNING )
+            job_state.running = True
+            return job_state
 
+        # TODO this is from the condor backend. What's the right thing for TES?
         if not job_running and job_state.running:
             log.debug( "(%s/%s) job has stopped running" % ( galaxy_id_tag, job_id ) )
             # Will switching from RUNNING to QUEUED confuse Galaxy?
@@ -161,23 +153,13 @@ class TESJobRunner( AsynchronousJobRunner ):
                 #if external_metadata:
                     #self._handle_metadata_externally( job_state.job_wrapper, resolve_requirements=True )
                 with open(job_state.output_file, 'w') as fh:
-                    try:
-                        fh.write(data['logs'][0]['stdout'])
-                    except:
-                        pass
+                    fh.write(self._concat_job_log(data, 'stdout'))
 
                 with open(job_state.error_file, 'w') as fh:
-                    try:
-                        fh.write(data['logs'][0]['stderr'])
-                    except:
-                        pass
+                    fh.write(self._concat_job_log(data, 'stderr'))
 
                 with open(job_state.exit_code_file, 'w') as fh:
-                    try:
-                        print data
-                        fh.write(data['logs'][0]['exitcode'])
-                    except:
-                        fh.write('0\n')
+                    fh.write(self._concat_exit_codes(data))
 
                 log.debug( "(%s/%s) job has completed" % ( galaxy_id_tag, job_id ) )
                 self.mark_as_finished(job_state)
@@ -185,22 +167,29 @@ class TESJobRunner( AsynchronousJobRunner ):
 
         if job_failed:
             log.debug( "(%s/%s) job failed" % ( galaxy_id_tag, job_id ) )
-            job_state.failed = True
             self.mark_as_failed(job_state)
-            return job_state
+            return
 
-        job_state.running = job_running
         return job_state
-
-    def finish_job(self, job_state):
-        super(TESJobRunner, self).finish_job(job_state)
 
     def stop_job( self, job ):
         """Attempts to delete a task from the task queue"""
-        external_id = job.job_runner_external_id
-        failure_message = condor_stop(external_id)
-        if failure_message:
-            log.debug("(%s/%s). Failed to stop tes %s" % (external_id, failure_message))
+
+        # Possibly the job was deleted before it was fully started.
+        # In this case, the job_id will be None. This seems to be a bug in Galaxy.
+        # It's likely that the job was in fact submitted to TES, but the job_id
+        # wasn't persisted to the monitor queue?
+        job_id = job.job_runner_external_id
+        if job_id is None:
+            return
+
+        master_addr = job.destination_params.get("tes_master_addr")
+        print '=' * 50
+        print 'stop job'
+        print job_id, master_addr
+        print
+        self._cancel_job(master_addr, job_id)
+        # TODO send cancel message to TES
 
     def recover( self, job, job_wrapper ):
         """Recovers jobs stuck in the queued/running state when Galaxy started"""
@@ -211,18 +200,18 @@ class TESJobRunner( AsynchronousJobRunner ):
         if job_id is None:
             self.put( job_wrapper )
             return
-        cjs = TESJobState( job_wrapper=job_wrapper, files_dir=self.app.config.cluster_files_directory )
-        cjs.job_id = str( job_id )
-        cjs.command_line = job.get_command_line()
-        cjs.job_wrapper = job_wrapper
-        cjs.job_destination = job_wrapper.job_destination
-        cjs.user_log = os.path.join( self.app.config.cluster_files_directory, 'galaxy_%s.tes.log' % galaxy_id_tag )
-        cjs.register_cleanup_file_attribute( 'user_log' )
+        job_state = TESJobState( job_wrapper=job_wrapper, files_dir=self.app.config.cluster_files_directory )
+        job_state.job_id = str( job_id )
+        job_state.command_line = job.get_command_line()
+        job_state.job_wrapper = job_wrapper
+        job_state.job_destination = job_wrapper.job_destination
+        job_state.user_log = os.path.join( self.app.config.cluster_files_directory, 'galaxy_%s.tes.log' % galaxy_id_tag )
+        job_state.register_cleanup_file_attribute( 'user_log' )
         if job.state == model.Job.states.RUNNING:
             log.debug( "(%s/%s) is still in running state, adding to the DRM queue" % ( job.id, job.job_runner_external_id ) )
-            cjs.running = True
-            self.monitor_queue.put( cjs )
+            job_state.running = True
+            self.monitor_queue.put( job_state )
         elif job.state == model.Job.states.QUEUED:
             log.debug( "(%s/%s) is still in DRM queued state, adding to the DRM queue" % ( job.id, job.job_runner_external_id ) )
-            cjs.running = False
-            self.monitor_queue.put( cjs )
+            job_state.running = False
+            self.monitor_queue.put( job_state )
